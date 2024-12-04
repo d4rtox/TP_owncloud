d579461e2ccc3ef1653d56ac5017b50005d7c591
d4rtox <168448479+d4rtox@users.noreply.github.com>
2024-12-04 10:54:27 +0100

    Update backups_ownclouds.sh

--- backups_ownclouds.sh
+++ backups_ownclouds.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#installation du pacquet zip :
+apt install zip
 
 # Variables
 SOURCE_CSV="/var/lib/docker/volumes/9e78b14313909f6318898c1c908b7d4faaf5553de74865ea522387cb6e7c5b1d/_data/data/max/files/toip/fichier.csv"
@@ -6,10 +8,6 @@ TOIP_DIR="/var/lib/docker/volumes/9e78b14313909f6318898c1c908b7d4faaf5553de74865
 BACKUP_DIR="$HOME/backups_ownclouds"
 TIMESTAMP=$(date +"%d-%m-%Y_%H:%M:%S")
 
-# Sauvegarder le fichier CSV avec le nom demandé
-BACKUP_CSV="$BACKUP_DIR/sio2-$TIMESTAMP.csv"
-cp "$SOURCE_CSV" "$BACKUP_CSV"
-echo "Fichier CSV sauvegardé sous : $BACKUP_CSV"
 
 # Compresser le contenu du répertoire `toip` en un fichier ZIP
 BACKUP_ZIP="$BACKUP_DIR/sio2-$TIMESTAMP.zip"
